e89462c0931e3e37e7dfe60616f27f9dc96d4628
Bennett Dungan <bennettdungan@gmail.com>
2020-06-08 00:14:29 -0500

    box styling lesson

--- App.js
+++ App.js
@@ -1,27 +1,75 @@
 import React from 'react'
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { 
+  View, 
+  Text, 
+  SafeAreaView, 
+  StyleSheet,
+  Platform,
+  StatusBar
+} from 'react-native'
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello baw!</Text>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={[styles.box, styles.cyan]}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.box, styles.blue]}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.box, styles.magenta]}>
+          <Text style={styles.text}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.box, styles.orange]}>
+          <Text style={styles.text}>Orange #cb4b16</Text>
+        </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  pink: {
-    backgroundColor: 'pink',
-  },
-  
-  container: {
-    margin: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
+  AndroidSafeArea: {
     flex: 1,
-  }
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  text: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
 })
 
 export default App
